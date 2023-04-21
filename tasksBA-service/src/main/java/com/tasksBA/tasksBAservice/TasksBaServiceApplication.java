5a1d1fc2cc4cfe49167c6668574607d6b41d734b
alexandru.bornaz <alexandru.bornaz@eos-it.ro>
2023-04-21 11:38:29 +0300

    project configured + database connectivity

---
+++ tasksBA-service/src/main/java/com/tasksBA/tasksBAservice/TasksBaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tasksBA.tasksBAservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksBaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksBaServiceApplication.class, args);
+	}
+
+}
---
+++ tasksBA-service/src/test/java/com/tasksBA/tasksBAservice/TasksBaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasksBA.tasksBAservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksBaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
