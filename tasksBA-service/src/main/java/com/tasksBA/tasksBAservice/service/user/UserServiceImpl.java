53f6bf424937af6b12d98e9df52e77cc4b912ab8
alexandru.bornaz <alexandru.bornaz@eos-it.ro>
2023-04-21 17:06:14 +0300

    user service with basic methods created

---
+++ tasksBA-service/src/main/java/com/tasksBA/tasksBAservice/service/user/UserServiceImpl.java
@@ -0,0 +1,46 @@
+package com.tasksBA.tasksBAservice.service.user;
+
+import com.tasksBA.tasksBAservice.model.User;
+import com.tasksBA.tasksBAservice.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class UserServiceImpl implements UserService{
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public boolean hasUserWithUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    @Override
+    public boolean hasUserWithEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(User user) {
+        userRepository.delete(user);
+    }
+}
